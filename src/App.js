a92d9f0d8115c60e1d37959773425f7582f63782
Mrinal kanti sardar <mrinalkantisardar007@gmail.com>
2022-07-15 15:50:15 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
+import About from "./components/About";
+import Dev from "./components/Dev";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Subscribe from "./components/Subscribe";
 import Twist from "./components/Twist";
 
 function App() {
@@ -7,6 +11,10 @@ function App() {
     <div>
       <Navbar />
       <Twist />
+      <About />
+      <Dev />
+      <Subscribe />
+      <Footer />
     </div>
   );
 }
